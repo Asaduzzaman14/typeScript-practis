0a9c12620c7dc6edb51d9a756fc317a2193142c1
Md Asaduzaman <asadasad9282@gmail.com>
2022-08-20 00:52:42 +0600

    custom object

--- user_defind_type/object-type.js
+++ user_defind_type/object-type.js
@@ -1,11 +1,17 @@
 var students;
 students = [];
 var user1;
-user1 = { userName: "asad", userId: 2 };
+user1 = { userName: "asad", userId: 1 };
 students.push(user1);
 var user2;
-user2 = { userName: "asad", userId: 2 };
+user2 = { userName: "Md asad", userId: 2 };
 students.push(user2);
-for (var key in students) {
-    console.log(students[key]["userName"]);
+var user3;
+user3 = { userName: "sakib", userId: 3 };
+students.push(user3);
+var getRequest;
+getRequest = "GET";
+function requesthandler(requestType) {
+    console.log(requestType);
 }
+requesthandler("GET");
--- user_defind_type/object-type.ts
+++ user_defind_type/object-type.ts
@@ -1,19 +1,35 @@
+type User = { userName: string, userId: number }
 
-
-let students: object[]
+let students: User[]
 students = [];
 
-let user1: { userName: string, userId: number };
-user1 = { userName: "asad", userId: 2 };
+
+let user1: User
+user1 = { userName: "asad", userId: 1 };
 students.push(user1)
 
 
-let user2: { userName: string, userId: number };
-user2 = { userName: "asad", userId: 2 };
+let user2: User;
+user2 = { userName: "Md asad", userId: 2 };
 
 students.push(user2)
 
-for (const key in students) {
-    console.log(students[key]["userName"]);
+
+let user3: User;
+user3 = { userName: "sakib", userId: 3 };
+
+students.push(user3)
+// console.log(students);
+
+
+type Requestype = "GET" | "POST"
+
+
+let getRequest: Requestype;
+
+getRequest = "GET";
+function requesthandler(requestType: Requestype) {
+    console.log(requestType);
 
 }
+requesthandler("GET")
